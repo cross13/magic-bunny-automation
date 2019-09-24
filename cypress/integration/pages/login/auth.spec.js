5c996487d9a1c01ae3f0b21fa9abcfaebf3a9ddf
Lucas Borella <lborella@onapsis.com>
2019-09-24 19:19:57 -0300

    Initial Commit

---
+++ cypress/integration/pages/login/auth.spec.js
@@ -0,0 +1,12 @@
+/// <reference types="Cypress" />
+
+context('Authenticate into the app', () => {
+    beforeEach(() => {
+      cy.visit('/');
+    })
+
+    it('Should render the correct login page', () => {
+
+    });
+
+});
